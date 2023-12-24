0d4b1ca5e2dc8feb124c15e9e6053439d9837e36
ksisovec <ksisovec@gmail.com>
2023-12-24 11:59:43 +0400

    add test message to department-service

---
+++ department-service/src/main/java/net/javaguides/departmentservice/controller/MessageController.java
@@ -0,0 +1,11 @@
+package net.javaguides.departmentservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @Value("$spring.boot.message")
+    private String message;
+}
